33426e2414309af7b686b9847ab62c6737d90ca0
xansiety <ferando543@outlook.com>
2022-11-05 17:27:44 -0600

    ReduxToolkit:Todos:RTK-Querys -> Trabajando con la informacion de la peticion HTTP devuelta por RTK mendiante el custom query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,24 @@
- 
+import { useGetTodosQuery } from "./store";
+
 export const TodoApp = () => {
+  const {data: todos , error, isLoading} = useGetTodosQuery() 
+     
   return (
     <>
-      <h1>Todos - RTK Query</h1>
+      <h1>Todos - RTK Querys</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
 
-      <pre>...</pre>
+      <pre> ... </pre>
+      <ul>
+        {
+            todos?.map(({id, title, completed}) => (
+                <li key={id}>
+                    <p>{title} - {(completed) ? 'Completado': 'Sin realizar'}</p> 
+                </li>
+            ))
+        }
+      </ul>
 
       <button>Next Todo</button>
     </>
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { todosApi } from './apis/todosApi'
 import { counterSlice, pokemonSlice } from './slices'
  
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
-    pokemon: pokemonSlice.reducer
+    pokemon: pokemonSlice.reducer, 
+    //RTK - QUERYS
+    [todosApi.reducerPath]: todosApi.reducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware)
 })
